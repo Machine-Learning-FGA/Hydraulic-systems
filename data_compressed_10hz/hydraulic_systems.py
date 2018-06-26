16a83301693abd30b65aa98fe5e49b6f10cea6ac
joaovitor3 <joaovytor0@gmail.com>
2018-06-26 00:36:15 -0300

    Adiciona python noteooks com novas métricas

--- data_compressed_10hz/hydraulic_systems.py
+++ data_compressed_10hz/hydraulic_systems.py
@@ -7,7 +7,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import timeit
 
+start = timeit.default_timer()
 
 teste_list = ['VS1', 'TS1', 'TS2', 'TS3', 'TS4','SE', 'PS1', 'PS2', 'PS3', 'PS4', 'PS5',
               'PS6', 'EPS1', 'CP']
@@ -42,3 +46,13 @@ knn = KNeighborsClassifier(n_neighbors=4)
 
 knn.fit(x_train, y_train.values.ravel())
 print(knn.score(x_test, y_test))
+pred = knn.predict(x_test)
+y_train_pred = knn.predict(x_train)
+cm_train = confusion_matrix(y_train, y_train_pred)
+cm_test = confusion_matrix(y_test, pred)
+print('confusion matrix teste\n', cm_test,'\n')
+print('confusion matrix treino\n',cm_train,'\n')
+print(classification_report(y_test, pred),'\n')
+stop = timeit.default_timer()
+
+print('Programa executado em ',stop - start,'segundos\n')
--- data_compressed_1hz/hydraulic_systems.py
+++ data_compressed_1hz/hydraulic_systems.py
@@ -7,7 +7,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+import timeit
 
+start = timeit.default_timer()
 
 teste_list = ['VS1', 'TS1', 'TS2', 'TS3', 'TS4','SE', 'PS1', 'PS2', 'PS3', 'PS4', 'PS5',
               'PS6', 'FS1', 'FS2', 'EPS1', 'CP']
@@ -31,14 +35,22 @@ for key in teste_list:
         master_df = pd.concat([master_df, df], axis=1)
 
 target_df = pd.read_table('profile.csv', header=None)
-target_df = target_df.drop([0,1,2,3], axis=1)
+target_df = target_df.drop([1,2,3,4], axis=1)
 
 print(master_df)
-# print(master_df.dtypes)
-# print(target_df.dtypes)
 
 x_train, x_test, y_train, y_test = train_test_split(master_df, target_df, test_size=0.3, random_state=42)
 knn = KNeighborsClassifier(n_neighbors=4)
 
 knn.fit(x_train, y_train.values.ravel())
 print(knn.score(x_test, y_test))
+pred = knn.predict(x_test)
+y_train_pred = knn.predict(x_train)
+cm_train = confusion_matrix(y_train, y_train_pred)
+cm_test = confusion_matrix(y_test, pred)
+print('confusion matrix teste\n', cm_test,'\n')
+print('confusion matrix treino\n',cm_train,'\n')
+print(classification_report(y_test, pred),'\n')
+stop = timeit.default_timer()
+
+print('Programa executado em ',stop - start,'segundos\n')
